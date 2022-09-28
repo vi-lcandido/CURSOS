fc3290e7ae915a09eb0ccfae61ea90c75f31563c
vi-lcandido <vitoria.lcandido@gmail.com>
2022-09-28 11:59:53 -0300

    arrumando o diretório beecrowd

--- OI/CODING-TANK/AULA3/COPA_BRASIL.js
+++
@@ -1,6 +0,0 @@
-let ganhouCopa = [1959, 1962, 1970, 1994, 2002]
-
-for (let index = 0; index < ganhouCopa.length; index++) {
-    console.log(`O brasil ganhou a copa de ${ganhouCopa[index]}`)
-    
-}
